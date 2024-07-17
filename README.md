167e15eef69e47c7148aecfc2a9ed0cfc21dcbd0
jonh-a <gravityrideseverything1@gmail.com>
2024-07-17 13:22:39 -0400

    add readme & time subcommand

---
+++ README.md
@@ -0,0 +1,40 @@
+# Cronometer Analysis
+
+CLI tool that provides an analysis of your Cronometer data.
+
+## Setup
+
+Clone the repo and install the project dependencies.
+```bash
+pip install -r requirements.txt
+```
+
+Next, export your nutrition data from Cronometer. You can do this by logging into [cronometer.com](https://www.cronometer.com), navigating to More > Account, and exporting your Daily Nutrition and Food & Recipe entires for the desired timeframe. 
+
+![image](img/cronometer_export.png)
+
+If you've been logging for a while, you may not be able to export the Food & Recipe entires for all time, so you may have to settle for a year.
+
+## Average micronutrients/day
+
+Returns your average quantity of each tracked micronutrient (glorified average of all columns).
+
+```bash
+python3 cronometer.py get-average --summary path/to/dailysummary.csv
+```
+
+Optionally, set a time frame (`--since="2023-12-25"`) or exclude outlier days under or over certain calorie threshold (`--disregard-under=1000 --disregard-above=2600`). Additionally, you can disregard days that were not marked as "complete" (`--complete-only`).
+
+```bash
+python3 cronometer.py get-average --summary path/to/dailysummary.csv --disregard-under=1000 --disregard-above=2600 --complete-only
+```
+
+## Micronutrients over time
+
+Returns a line graph displaying the daily micronutrient over time. Provide the micronutrient you wish to track with the `--track` argument.
+
+```bash
+python3 cronometer.py time --summary path/to/dailysummary.csv --track="Energy (kcal)"
+```
+
+![image](img/over_time.png)
--- cronometer.py
+++ cronometer.py
@@ -2,6 +2,8 @@ import pandas as pd
 from rich import print_json
 import argparse
 import json
+import asciichartpy
+import shutil
 
 
 def get_args():
@@ -9,14 +11,21 @@ def get_args():
         prog="cronometer analyzer",
         description="analyzes your cronometer data",
     )
+    
+    subparsers = parser.add_subparsers(dest='subcommand')
+    average_parser = subparsers.add_parser('get-average')
+    average_parser.add_argument('--summary', required=True, help="path to dailysummary.csv")
+    average_parser.add_argument('--complete-only', action='store_true', help="calculate using complete days only")
+    average_parser.add_argument('--disregard-above', help="disregard days over N calories", type=int)
+    average_parser.add_argument('--disregard-under', help="disregard days under N calories", type=int)
+    average_parser.add_argument('--since', help="calculate using days since (YYYY-MM-DD)")
+    average_parser.add_argument('--json', action='store_true', help="return json output", default=True)
 
-    parser.add_argument('--summary', required=True, help="path to dailysummary.csv")
-    parser.add_argument('--foods', required=False, help="path to servings.csv")
-    parser.add_argument('--complete-only', action='store_true', help="calculate using complete days only")
-    parser.add_argument('--disregard-under', help="disregard days under N calories", type=int)
-    parser.add_argument('--disregard-above', help="disregard days over N calories", type=int)
-    parser.add_argument('--since', help="calculate using days since (YYYY-MM-DD)")
-    parser.add_argument('--json', action='store_true', help="return json output", default=True)
+    time_parser = subparsers.add_parser('time')
+    time_parser.add_argument('--summary', required=True, help="path to dailysummary.csv")
+    time_parser.add_argument('--complete-only', action='store_true', help="calculate using complete days only")
+    time_parser.add_argument('--since', help="calculate using days since (YYYY-MM-DD)")
+    time_parser.add_argument('--track', help="nutrient to track", action='append', required=True)
 
     return parser.parse_args()
 
@@ -55,43 +64,77 @@ def filter_since(data, since):
     return data[data["Date"] >= since]
 
 
-def calculate_daily_avg(data):
+def filter_by_nutrient(data, nutrients):
+    keep_columns = [*nutrients, "Date"]
+    return data.filter(items=keep_columns)
+
+
+def calculate_total_avg(data):
     invalid_fieldnames = ["Date", "Completed", "_foods"]
     valid_columns = [col for col in data.columns if col not in invalid_fieldnames]
     
-    daily_avgs = round(data[valid_columns].mean(), 2).to_dict()
-    return daily_avgs
+    total_avgs = round(data[valid_columns].mean(), 2).to_dict()
+    return total_avgs
 
 
-if __name__ == "__main__":
-    args = get_args()
-
+def get_average(args):
     data = parse_csv(args.summary)
-
-    if args.foods:
-        servings_df = parse_csv(args.foods)
-        data = merge_summary_and_foods_data(data, servings_df)
     
     if args.complete_only:
         data = filter_complete(data)
-
     if args.disregard_under:
         data = filter_out_under(data, args.disregard_under)
-
     if args.disregard_above:
         data = filter_out_over(data, args.disregard_above)
-
     if args.since:
         data = filter_since(data, args.since)
 
-    daily_avgs = calculate_daily_avg(data)
+    total_avgs = calculate_total_avg(data)
 
-    parsed_data = {
-        "averages": {
-            "daily_averages": daily_avgs,
-        }
-    }
+    parsed_data = {"total_avgs": total_avgs}
 
     if args.json:
         print_json(json.dumps(parsed_data))
         exit(0)
+
+
+def track_nutrients_over_time(args):
+    data = parse_csv(args.summary)
+    
+    if args.complete_only:
+        data = filter_complete(data)
+    if args.since:
+        data = filter_since(data, args.since)
+
+    filtered_data = filter_by_nutrient(data, args.track)
+    
+    plot_nutrients(filtered_data, args.track)
+
+
+def plot_nutrients(data, nutrients):
+    terminal_size = shutil.get_terminal_size((80, 20))
+    width = terminal_size.columns - 20 
+    height = terminal_size.lines - 5 
+
+    for nutrient in nutrients:
+        dates = pd.to_datetime(data['Date']).dt.strftime('%Y-%m-%d').tolist()
+        values = data[nutrient].tolist()
+        
+        if len(values) > width:
+            factor = len(values) // width
+            condensed_values = values[::factor]
+        else:
+            condensed_values = values
+        
+        print(f"\n{nutrient} over time ({dates[0]} - {dates[-1]})")
+
+        print(asciichartpy.plot(condensed_values, {'height': height}))
+
+
+if __name__ == "__main__":
+    args = get_args()
+    
+    if args.subcommand == 'get-average':
+        get_average(args)
+    elif args.subcommand == 'time':
+        track_nutrients_over_time(args)
